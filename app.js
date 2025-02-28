3d68531f49672dad52639f8c0082cd7fbb80e441
mario-alexx <marioatoroh@gmail.com>
2025-02-28 12:48:32 -0300

    removing some comments

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
 function toggleMenu() {
     document.querySelector('.menu').classList.toggle('active');
 }
-
-// const slider = document.querySelector('slider');
-// let position = 0;
-// const itemWidth = slider.children[0].offsetWidth;
-
-// function moveSlider() {
-//     position -= itemWidth;
-//     if (position < -itemWidth) {
-//         position = 0;
-//     }
-//     slider.style.transform = `translateX(${position}px)`;
-// }
-
-// setInterval(moveSlider, 2000); // Cambia cada 2 segundos
