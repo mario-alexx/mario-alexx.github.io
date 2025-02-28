9b1e18dd6e0287a298f27bcd5aba98a2be2b870b
mario-alexx <marioatoroh@gmail.com>
2025-02-28 12:44:22 -0300

    adding responsive design

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const slider = document.getElementById('slider');
-let position = 0;
-const itemWidth = slider.children[0].offsetWidth;
-
-function moveSlider() {
-    position -= itemWidth;
-    if (position < -itemWidth) {
-        position = 0;
-    }
-    slider.style.transform = `translateX(${position}px)`;
+function toggleMenu() {
+    document.querySelector('.menu').classList.toggle('active');
 }
 
-setInterval(moveSlider, 2000); // Cambia cada 2 segundos
+// const slider = document.querySelector('slider');
+// let position = 0;
+// const itemWidth = slider.children[0].offsetWidth;
+
+// function moveSlider() {
+//     position -= itemWidth;
+//     if (position < -itemWidth) {
+//         position = 0;
+//     }
+//     slider.style.transform = `translateX(${position}px)`;
+// }
+
+// setInterval(moveSlider, 2000); // Cambia cada 2 segundos
